ce2566a045543337a63405790e5ea8078b8a505b
Diana <edifactdianadiaz@gmail.com>
2025-04-07 13:42:34 -0600

    Subi Cambios Ara

--- src/main/java/mx/com/edifact/gitdemo/GitDemo.java
+++ src/main/java/mx/com/edifact/gitdemo/GitDemo.java
@@ -9,6 +9,8 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello World java 21");
         String name = "Roger Azcorra";
+        int edad = 14;
         System.out.println("Nombre: " + name);
+        System.out.print(edad);
     }
 }
